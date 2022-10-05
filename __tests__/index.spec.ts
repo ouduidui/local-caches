93e5665ed7d4294d57515470141645520867a820
oushihao <oushihao@bytedance.com>
2022-10-05 20:17:51 +0800

    feat: implement basic cache feature
    Change-Id: Ib92f21642aec2d2cab0b391ea543e56db7e952e8

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,6 +1,85 @@
+import fs from 'fs'
+import path from 'path'
+import rimraf from 'rimraf'
 import { expect, it } from 'vitest'
-import { saySomething } from '~/foo'
+import { clearCaches, getCache, getCaches, removeCache, setCache } from 'dist/index'
 
-it('happy path', () => {
-  expect(saySomething('world')).toMatchSnapshot('hello, world')
+it('should create file when set caches', () => {
+  if (fs.existsSync(path.resolve(process.cwd(), '.cache')))
+    rimraf.sync(path.resolve(process.cwd(), '.cache'))
+
+  expect(fs.existsSync(path.resolve(process.cwd(), '.cache', 'index'))).toBeFalsy()
+  setCache('key', 'val')
+  expect(fs.existsSync(path.resolve(process.cwd(), '.cache', 'index'))).toBeTruthy()
+})
+
+it('should get caches', () => {
+  if (fs.existsSync(path.resolve(process.cwd(), '.cache')))
+    rimraf.sync(path.resolve(process.cwd(), '.cache'))
+
+  setCache('key1', 'val1')
+  setCache('key2', 'val2')
+  expect(getCache('key1')).toBe('val1')
+  expect(getCache('key2')).toBe('val2')
+  expect(getCaches()).toStrictEqual({ key1: 'val1', key2: 'val2' })
+})
+
+it('should cover cache', () => {
+  if (fs.existsSync(path.resolve(process.cwd(), '.cache')))
+    rimraf.sync(path.resolve(process.cwd(), '.cache'))
+
+  setCache('key1', 'val1')
+  expect(getCache('key1')).toBe('val1')
+  setCache('key1', 'val2')
+  expect(getCache('key1')).toBe('val2')
+  expect(getCaches()).toStrictEqual({ key1: 'val2' })
+})
+
+it('should delete cache', () => {
+  if (fs.existsSync(path.resolve(process.cwd(), '.cache')))
+    rimraf.sync(path.resolve(process.cwd(), '.cache'))
+
+  setCache('key1', 'val1')
+  setCache('key2', 'val2')
+  expect(getCaches()).toStrictEqual({ key1: 'val1', key2: 'val2' })
+  removeCache('key1')
+  expect(getCaches()).toStrictEqual({ key2: 'val2' })
+  removeCache('key2')
+  expect(getCaches()).toStrictEqual({})
+})
+
+it('should clear caches', () => {
+  if (fs.existsSync(path.resolve(process.cwd(), '.cache')))
+    rimraf.sync(path.resolve(process.cwd(), '.cache'))
+
+  setCache('key1', 'val1')
+  setCache('key2', 'val2')
+  expect(getCaches()).toStrictEqual({ key1: 'val1', key2: 'val2' })
+  clearCaches()
+  expect(getCaches()).toBeNull()
+  expect(fs.existsSync(path.resolve(process.cwd(), '.cache', 'index'))).toBeFalsy()
+  expect(fs.existsSync(path.resolve(process.cwd(), '.cache'))).toBeFalsy()
+})
+
+it('should set path name', () => {
+  const pathName = '.store'
+
+  if (fs.existsSync(path.resolve(process.cwd(), pathName)))
+    rimraf.sync(path.resolve(process.cwd(), pathName))
+  expect(fs.existsSync(path.resolve(process.cwd(), pathName, 'index'))).toBeFalsy()
+  setCache('key', 'val', { pathName })
+  expect(fs.existsSync(path.resolve(process.cwd(), pathName, 'index'))).toBeTruthy()
+
+  rimraf.sync(path.resolve(process.cwd(), pathName))
+})
+
+it('should set file name', () => {
+  const fileName = 'store'
+
+  if (fs.existsSync(path.resolve(process.cwd(), '.cache')))
+    rimraf.sync(path.resolve(process.cwd(), '.cache'))
+
+  expect(fs.existsSync(path.resolve(process.cwd(), '.cache', fileName))).toBeFalsy()
+  setCache('key', 'val', { fileName })
+  expect(fs.existsSync(path.resolve(process.cwd(), '.cache', fileName))).toBeTruthy()
 })
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,86 @@
-import { saySomething } from './foo'
+import fs from 'fs'
+import path from 'path'
 
-// eslint-disable-next-line no-console
-console.log(saySomething('world'))
+const ROOT_PATH = process.cwd()
 
-export default { saySomething }
+const LINE = /(?:^|^)\s*(?:export\s+)?([\w.-]+)(?:\s*=\s*?|:\s+?)(\s*'(?:\\'|[^'])*'|\s*"(?:\\"|[^"])*"|\s*`(?:\\`|[^`])*`|[^#\r\n]+)?\s*(?:#.*)?(?:$|$)/i
+
+interface CachesConfig {
+  pathName?: `.${string}`
+  fileName?: string
+}
+
+const DEFAULT_CONFIG: Required<CachesConfig> = {
+  pathName: '.cache',
+  fileName: 'index',
+}
+
+const existPath = ({ pathName = DEFAULT_CONFIG.pathName }: CachesConfig = {}) => fs.existsSync(path.resolve(ROOT_PATH, pathName))
+
+const existFile = ({ fileName = DEFAULT_CONFIG.fileName, pathName = DEFAULT_CONFIG.pathName }: CachesConfig = {}) =>
+  existPath(pathName) && fs.existsSync(path.resolve(ROOT_PATH, pathName, fileName))
+
+const parseCaches = ({ fileName = DEFAULT_CONFIG.fileName, pathName = DEFAULT_CONFIG.pathName }: CachesConfig = {}) => {
+  const lines = fs.readFileSync(path.resolve(ROOT_PATH, pathName, fileName), 'utf-8')
+    .replace(/\r\n?/mg, '\n')
+    .split('\n')
+  const caches = new Map<string, string>()
+  for (const line of lines) {
+    const match = LINE.exec(line)
+    if (!match) continue
+    caches.set(match[1], match[2])
+  }
+
+  return caches
+}
+
+const setCaches = (
+  caches: Map<string, string>,
+  {
+    fileName = DEFAULT_CONFIG.fileName,
+    pathName = DEFAULT_CONFIG.pathName,
+  }: CachesConfig = {}) => {
+  const lines: string[] = []
+  caches.forEach((value, key) => lines.push(`${key}=${value}`))
+  fs.writeFileSync(path.resolve(ROOT_PATH, pathName, fileName), lines.join('\n'))
+}
+
+export const getCaches = (config?: CachesConfig) => {
+  if (!existFile(config)) return null
+  return Object.fromEntries(parseCaches(config))
+}
+
+export const getCache = (key: string, config?: CachesConfig) => {
+  if (!existFile(config)) return undefined
+  return parseCaches(config).get(key)
+}
+
+export const setCache = (key: string, value: string, config?: CachesConfig) => {
+  const pathName = config?.pathName ?? DEFAULT_CONFIG.pathName
+  const fileName = config?.fileName ?? DEFAULT_CONFIG.fileName
+  if (!existPath(config))
+    fs.mkdirSync(path.resolve(ROOT_PATH, pathName))
+  if (!existFile(config))
+    fs.writeFileSync(path.resolve(ROOT_PATH, pathName, fileName), '')
+
+  const caches = parseCaches(config)
+  caches.set(key, value)
+  setCaches(caches, config)
+}
+
+export const removeCache = (key: string, config?: CachesConfig) => {
+  if (!existFile(config)) return
+  const caches = parseCaches(config)
+  caches.delete(key)
+  setCaches(caches, config)
+}
+
+export const clearCaches = (config?: CachesConfig) => {
+  if (!existFile(config)) return
+  const pathName = config?.pathName ?? DEFAULT_CONFIG.pathName
+  const fileName = config?.fileName ?? DEFAULT_CONFIG.fileName
+  fs.unlinkSync(path.resolve(ROOT_PATH, pathName, fileName))
+
+  if (!fs.readdirSync(path.resolve(ROOT_PATH, pathName)).length)
+    fs.rmdirSync(path.resolve(ROOT_PATH, pathName))
+}
